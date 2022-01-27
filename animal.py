076c0810a8c9490dafc0023b6b15b5bebf580d93
Aram Avagyan <91014640+AramYe@users.noreply.github.com>
2022-01-27 13:30:53 +0000

    Complete class Animal

--- animal.py
+++ animal.py
@@ -1,11 +1,10 @@
-
-
 class Animal:
 
     def __init__(self, species: str):
-
         self.species = species
+    
+    def show_species(self):
+        print(f"I am an ordinary animal")
 
-   
-
-    pass
+    def make_sound(self):
+        print("Grrr")
--- cat.py
+++ cat.py
@@ -2,4 +2,4 @@ from animal import Animal
 
 class Cat(Animal):
 
-    pass
+    
